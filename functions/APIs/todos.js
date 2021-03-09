25da863735b8a919def225e985897bc4f7d03d1d
Sean LaFlam <sean.laflam@gmail.com>
2021-03-09 16:05:53 -0500

    Created Utils

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,1 +1,15 @@
-
+exports.getAllTodos = (request, response) => {
+    todos = [
+        {
+            'id': '1',
+            'title': 'greeting',
+            'body': 'Hello world from Sean' 
+        },
+        {
+            'id': '2',
+            'title': 'greeting2',
+            'body': 'Hello2 world2 from Sean' 
+        }
+    ]
+    return response.json(todos);
+}
